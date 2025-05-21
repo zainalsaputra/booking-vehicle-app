2b4899e6d7fac0ba060e5ddd2f5b29f314a3e0bb
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2025-05-21 11:01:36 +0700

    drop cascade constraint vehicle_types

--- backend/database/migrations/2025_05_19_182112_create_vehicle_types_table.php
+++ backend/database/migrations/2025_05_19_182112_create_vehicle_types_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vehicle_types');
+       DB::statement('DROP TABLE IF EXISTS vehicle_types CASCADE');
     }
 };
